48dbe6ecbfc30c19ec4c0f961d21f541af4109fc
Larissa Albantakis <Larissa Albantakis>
2014-11-19 12:00:21 -0600

    Change Makefile source directory to new name

--- Makefile.nix
+++ Makefile.nix
@@ -2,7 +2,7 @@ default: compile
 
 # Compile everything into an executable called main.o in the build directory
 compile:
-	c++ src/*.cpp -o build/main.o
+	c++ temporalSpatialIntegrationLite/*.cpp -o build/main.o
 
 # Remove the built object
 clean:
